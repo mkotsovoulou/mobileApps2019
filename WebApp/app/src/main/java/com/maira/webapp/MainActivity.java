1a347212d37af6b7b811a3144a6a4ca40e8ac226
Mairako <mkotsovoulou@gmail.com>
2019-02-07 22:36:38 +0200

    menu2

--- WebApp/app/src/main/java/com/maira/webapp/MainActivity.java
+++ WebApp/app/src/main/java/com/maira/webapp/MainActivity.java
@@ -4,22 +4,29 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    WebView w;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView w = (WebView) findViewById(R.id.webv);
+        w = (WebView) findViewById(R.id.webv);
         WebSettings webSettings = w.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        w.loadUrl("http://mairak.students.acg.edu/webviews/dd/playDice.html");
+        w.loadUrl("http://mairak.students.acg.edu/webviews/dice/playDice.html");
 
 
         w.setWebViewClient(new WebViewClient() {
@@ -34,5 +41,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
+
+    public void refresh(View v) {
+
+        w.reload();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.refreshMenu:
+                w.reload();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
